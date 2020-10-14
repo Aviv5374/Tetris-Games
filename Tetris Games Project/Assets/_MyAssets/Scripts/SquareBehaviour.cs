919846bbe2f881cc05fa19c6052f4ab16d0cdfa0
Aviv5374 <aviv5374@gmail.com>
2020-10-14 16:03:17 +0300

    Brainstorming In Engine 2

--- Tetris Games Project/Assets/_MyAssets/Scripts/SquareBehaviour.cs
+++ Tetris Games Project/Assets/_MyAssets/Scripts/SquareBehaviour.cs
@@ -44,28 +44,49 @@ public class SquareBehaviour : MonoBehaviour, IPointerEnterHandler
         //}
     }
 
-        #endregion
+    #endregion
 
-        //private void MousePositionTracker()
-        //{
-        //    //Store start position to move from, based on objects current transform position.
-        //    Vector2 start = transform.position;
-        //    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //    mousePosition.z = transform.position.z;
-        //    transform.position = Vector2.MoveTowards(start, mousePosition, speed * Time.deltaTime);
-        //}
-
-
-        //private void AttemptToPlaceDefenderAt(Vector2 gridPos)
-        //{
-        //    var starDisplay = FindObjectOfType<StarDisplay>();
-        //    int defenderCost = defender.StarCost;
-        //    if (starDisplay.HaveEnoughStars(defenderCost))
-        //    {
-        //        BlockSpawner(gridPos);
-        //        starDisplay.SpendStars(defenderCost);
-        //    }
-        //}
+    //private void MousePositionTracker()
+    //{
+    //    //Store start position to move from, based on objects current transform position.
+    //    Vector2 start = transform.position;
+    //    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    mousePosition.z = transform.position.z;
+    //    transform.position = Vector2.MoveTowards(start, mousePosition, speed * Time.deltaTime);
+    //}
 
 
-    }
+    //private void AttemptToPlaceDefenderAt(Vector2 gridPos)
+    //{
+    //    var starDisplay = FindObjectOfType<StarDisplay>();
+    //    int defenderCost = defender.StarCost;
+    //    if (starDisplay.HaveEnoughStars(defenderCost))
+    //    {
+    //        BlockSpawner(gridPos);
+    //        starDisplay.SpendStars(defenderCost);
+    //    }
+    //}
+
+    //private Vector2 GetSquareClicked()
+    //{
+    //    Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    //    Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+    //    Vector2 gridPos = SnapToGrid(worldPos);
+    //    return gridPos;
+    //}
+
+    //private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    //{
+    //    float newX = Mathf.RoundToInt(rawWorldPos.x);
+    //    float newY = Mathf.RoundToInt(rawWorldPos.y);
+    //    return new Vector2(newX, newY);
+    //}
+
+    //private void BlockSpawner(Vector2 gridPos)
+    //{
+    //    Defender newDefender = Instantiate(defender, gridPos, Quaternion.identity);
+    //    newDefender.transform.parent = defenderParent.transform;
+    //}
+
+
+}
